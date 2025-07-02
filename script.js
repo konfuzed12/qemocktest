7ceed19a85baa50360b5acd9ed783b2f57893e6c
Aj <ajadao12@gmail.com>
2025-07-03 01:28:16 +0800

    new questions

--- script.js
+++ script.js
@@ -1,60 +1,69 @@
 const Questions = [
     {
-    question: "Computer system is divided into how many numbers of components?",
+    question: "Which of the following is a PHP framework?",
     answers: [
-        { text: "One", correct: false },
-        { text: "Two", correct: false },
-        { text: "Three", correct: false },
-        { text: "Four", correct: true }
+        { text: "Laravel", correct: true },
+        { text: "React", correct: false },
+        { text: "Tailwind", correct: false },
+        { text: "Bootstrap", correct: false }
     ]
 },
 {
-    question: "What is the name of the operating system that reads and reacts in terms of actual time?",
+    question: "It stores data in terms of entities and the relationships between entities.",
     answers: [
-        { text: "Real time system", correct: true },
-        { text: "Time sharing system", correct: false },
-        { text: "Quick response system", correct: false },
-        { text: "Batch system", correct: false }
+        { text: "Graph Databases", correct: true },
+        { text: "Cloud Databases", correct: false },
+        { text: "Data Warehouses", correct: false },
+        { text: "NoSQL Databases", correct: false }
     ]
 },
 {
-    question: "Which of the following software is used to simplify using of system software?",
+    question: "It is a last in-first out (LIFO) data structure in which all insertions and deletions are restricted to one end, called the top.",
     answers: [
-        { text: "Time sharing system", correct: false },
-        { text: "Multi-tasking system", correct: false },
-        { text: "Operating environment", correct: true },
-        { text: "Spreadsheet", correct: false }
+        { text: "Queue", correct: false },
+        { text: "Dequeue", correct: false },
+        { text: "Pop", correct: false },
+        { text: "Stacks", correct: true }
     ]
 },
 {
-    question: "UNIX operating system",
+    question: "This method is used by small programming teams for small to medium-sized systems.",
     answers: [
-        { text: "Can run along PC’s and larger system", correct: false },
-        { text: "Is multitasking", correct: false },
-        { text: "Is multiuser", correct: false },
-        { text: "All of these", correct: true }
+        { text: "Agile Method", correct: true },
+        { text: "Waterfall Model", correct: false },
+        { text: "V-Model", correct: false },
+        { text: "RAD Model", correct: false }
     ]
 },
 {
-    question: "Express the statement in propositional formula, 'The campus server does not work if the internet is off.' \nWhere: p: internet is on \n the campus server works",
+    question: "What is the foundation of computer science that focuses on concepts and reasoning methods?",
     answers: [
-        { text: "p ∨ q", correct: false },
-        { text: "p → q", correct: false },
-        { text: "¬p ∨ ¬q", correct: false },
-        { text: "¬p → ¬q", correct: true }
+        { text: "Integral Calculus", correct: false },
+        { text: "Physics", correct: false },
+        { text: "Discrete Mathematics", correct: true },
+        { text: "Applied Physics", correct: false }
     ]
 },
 {
-    question: "Let the domain of x be all plants and animals. Which of the following is true if:\nB(x) = 'x is a bird'\nE(x) = 'x lays eggs'\nM(x) = 'x is a mammal'\nP(x) = 'x is a plant'\nW(x) = 'x inhabits water'",
+    question: "The fields that include the identifier attributes of the table",
     answers: [
-        { text: "∀x (E(x) → B(x))", correct: false },
-        { text: "∃x (W(x) ∧ ¬P(x))", correct: true },
-        { text: "∃x (W(x) ∧ P(x))", correct: false },
-        { text: "∀x (W(x) ∧ ¬M(x))", correct: false }
+        { text: "Foreign key", correct: false },
+        { text: "Concatenated keys", correct: false },
+        { text: "Borrowed keys", correct: false },
+        { text: "Primary key", correct: true }
     ]
 },
 {
-    question: "Express the statement in predicate logic, 'There is someone who is loved by everyone.'\nLet L(x,y) = 'x loves y'",
+    question: "Which of the following is not a data type?",
+    answers: [
+        { text: "Floating point", correct: false },
+        { text: "Character", correct: false },
+        { text: "Header", correct: true },
+        { text: "Integer", correct: false }
+    ]
+},
+{
+    question: "Express in predicate logic: 'There is someone who is loved by everyone.' Let L(x,y) = 'x loves y'",
     answers: [
         { text: "∀x ∀x", correct: false },
         { text: "∃x ∃x", correct: false },
@@ -63,12 +72,382 @@ const Questions = [
     ]
 },
 {
-    question: "Let A = {0,2,5,6,7} and let B = {1,2,3,4,6}. Which of the given relations have the set {(2,2), (6,6)}?",
+    question: "It is a proposition form that is always true regardless of the truth values of the individual propositions substituted for its proposition values.",
     answers: [
-        { text: "R = {(a,b) ∈ A×B | a = b}", correct: true },
-        { text: "R = {(a,b) ∈ A×B | a < b}", correct: false },
-        { text: "R = {(a,b) ∈ A×B | a > b}", correct: false },
-        { text: "R = {(a,b) ∈ A×B | a ≠ b}", correct: false }
+        { text: "Contradiction", correct: false },
+        { text: "De Morgan’s Laws", correct: false },
+        { text: "Rules of Inference", correct: false },
+        { text: "Tautology", correct: true }
+    ]
+},
+{
+    question: "This software engineering principle promotes simplicity.",
+    answers: [
+        { text: "YAGNI", correct: false },
+        { text: "DRY", correct: false },
+        { text: "KISS", correct: true },
+        { text: "SRP", correct: false }
+    ]
+},
+{
+    question: "Color order of a Straight Through Cable",
+    answers: [
+        { text: "White-Orange, Orange, White-Green, Blue, White-Blue, Green, White-Brown, Brown", correct: true },
+        { text: "White-Green, Green, White-Orange, Blue, White-Blue, Orange, White-Brown, Brown", correct: false },
+        { text: "White-Blue, Blue, White-Green, White-Orange, Orange, Green, White-Brown, Brown", correct: false },
+        { text: "White-Orange, Orange, White-Green, Green, Blue, White-Blue, Brown, White-Brown", correct: false }
+    ]
+},
+{
+    question: "Let A = {0,2,5,6,7} and B = {1,2,3,4,6}. Which relation includes (2,2) and (6,6)?",
+    answers: [
+        { text: "a=b", correct: true },
+        { text: "a<b", correct: false },
+        { text: "a>b", correct: false },
+        { text: "a≠b", correct: false }
+    ]
+},
+{
+    question: "This clause is used to give a table, or a column in a table, a temporary name.",
+    answers: [
+        { text: "AND", correct: false },
+        { text: "AS", correct: true },
+        { text: "AVG", correct: false },
+        { text: "LIKE", correct: false }
+    ]
+},
+{
+    question: "An ordered collection of data items of the same type referred to collectively by a single name.",
+    answers: [
+        { text: "Array", correct: true },
+        { text: "List", correct: false },
+        { text: "Pointer", correct: false },
+        { text: "Record", correct: false }
+    ]
+},
+{
+    question: "This Photoshop bar/menu gives the user the option to switch between different default workspaces.",
+    answers: [
+        { text: "File Image Name", correct: false },
+        { text: "Application Bar", correct: false },
+        { text: "Workspace Switcher Menu", correct: true },
+        { text: "Options Bar", correct: false }
+    ]
+},
+{
+    question: "What is the meaning of API?",
+    answers: [
+        { text: "Artificial Programming IntelliSense", correct: false },
+        { text: "Application Processing and Integration", correct: false },
+        { text: "Application Programming Interface", correct: true },
+        { text: "Application Processor Interface", correct: false }
+    ]
+},
+{
+    question: "An ordered set consisting of a variable number of elements to which additions and deletions may be made",
+    answers: [
+        { text: "Array", correct: false },
+        { text: "List", correct: true },
+        { text: "Pointer", correct: false },
+        { text: "Record", correct: false }
+    ]
+},
+{
+    question: "It transmits the unstructured, raw bit stream over a physical medium.",
+    answers: [
+        { text: "Application Layer", correct: false },
+        { text: "Physical Layer", correct: true },
+        { text: "Presentation Layer", correct: false },
+        { text: "Data-link Layer", correct: false }
+    ]
+},
+{
+    question: "It shows how data is processed by a system in terms of inputs and outputs.",
+    answers: [
+        { text: "Flowchart", correct: false },
+        { text: "Data Flow Diagram", correct: true },
+        { text: "Role Activity Diagram", correct: false },
+        { text: "Gantt Chart", correct: false }
+    ]
+},
+{
+    question: "A byte is composed of how many bits (binary digits)?",
+    answers: [
+        { text: "12 bits", correct: false },
+        { text: "7 bits", correct: false },
+        { text: "9 bits", correct: false },
+        { text: "8 bits", correct: true }
+    ]
+},
+{
+    question: "It is restarting the computer and allows the system to load OS again, which means that the system is already turned on from the beginning.",
+    answers: [
+        { text: "Soft Booting", correct: true },
+        { text: "Inputting", correct: false },
+        { text: "Booting", correct: false },
+        { text: "None of the above", correct: false }
+    ]
+},
+{
+    question: "This type of text is meant to explain, inform, or describe.",
+    answers: [
+        { text: "Argumentative Texts", correct: false },
+        { text: "Narrative Texts", correct: false },
+        { text: "Subtitle", correct: false },
+        { text: "Expository Texts", correct: true }
+    ]
+},
+{
+    question: "It is connected via telephone lines or radio waves.",
+    answers: [
+        { text: "WAN", correct: false },
+        { text: "LAN", correct: true },
+        { text: "MAN", correct: false },
+        { text: "PAN", correct: false }
+    ]
+},
+{
+    question: "It is a collection of hardware components interconnected via protocols to share data.",
+    answers: [
+        { text: "Computer Network", correct: true },
+        { text: "Signal", correct: false },
+        { text: "Data Transmission", correct: false },
+        { text: "Server", correct: false }
+    ]
+},
+{
+    question: "Which of the following is not a command that comes under DML?",
+    answers: [
+        { text: "DELETE", correct: false },
+        { text: "SELECT", correct: false },
+        { text: "ALTER", correct: true },
+        { text: "UPDATE", correct: false }
+    ]
+},
+{
+    question: "It is the process of designing and building something that serves a particular purpose.",
+    answers: [
+        { text: "Architecture", correct: false },
+        { text: "Engineering", correct: true },
+        { text: "Programming", correct: false },
+        { text: "Testing", correct: false }
+    ]
+},
+{
+    question: "Who invented the World Wide Web?",
+    answers: [
+        { text: "Alan Turing", correct: false },
+        { text: "Rasmus Lerdorf", correct: false },
+        { text: "Tim Berners-Lee", correct: true },
+        { text: "Morten Hjerde", correct: false }
+    ]
+},
+{
+    question: "Express the statement in propositional formula: 'The campus server does not work if the internet is off.' Where p = 'The internet is on' and q = 'The campus server works'.",
+    answers: [
+        { text: "p ∨ q", correct: false },
+        { text: "p → q", correct: false },
+        { text: "¬p ∨ ¬q", correct: false },
+        { text: "¬p → ¬q", correct: true }
+    ]
+},
+{
+    question: "What is a compact and portable electronic storage device?",
+    answers: [
+        { text: "Floppy Diskette", correct: false },
+        { text: "Flash Drive", correct: true },
+        { text: "Central Processing Unit", correct: false },
+        { text: "CD-ROM", correct: false }
+    ]
+},
+{
+    question: "Which of the following is the first phase of UX Design Process?",
+    answers: [
+        { text: "Specify Requirements", correct: false },
+        { text: "Testing", correct: false },
+        { text: "Discovery", correct: true },
+        { text: "Ideation", correct: false }
+    ]
+},
+{
+    question: "It refers to the number of pixels in the monitor image",
+    answers: [
+        { text: "Media", correct: false },
+        { text: "Display", correct: false },
+        { text: "Resolution", correct: true },
+        { text: "Bandwidth", correct: false }
+    ]
+},
+{
+    question: "It increases the speed of the processor by recording and anticipating instructions.",
+    answers: [
+        { text: "Cache memory", correct: true },
+        { text: "Read only memory", correct: false },
+        { text: "Random access memory", correct: false },
+        { text: "Computer memory", correct: false }
+    ]
+},
+{
+    question: "An element of a list, usually implemented as an object that contains a reference to another object of the same type.",
+    answers: [
+        { text: "Nodes", correct: true },
+        { text: "Linked list", correct: false },
+        { text: "Pointer", correct: false },
+        { text: "Array", correct: false }
+    ]
+},
+{
+    question: "It is a startup sequence that starts the operating system of a computer when it is turned on",
+    answers: [
+        { text: "Reformatting", correct: false },
+        { text: "Inputting", correct: false },
+        { text: "Booting", correct: true },
+        { text: "None of the above", correct: false }
+    ]
+},
+{
+    question: "The response is displayed in a new window or tab",
+    answers: [
+        { text: "_self", correct: false },
+        { text: "_top", correct: false },
+        { text: "_blank", correct: true },
+        { text: "Framename", correct: false }
+    ]
+},
+{
+    question: "It is when what is being viewed is large and takes up most of the screen",
+    answers: [
+        { text: "Medium close-up", correct: true },
+        { text: "Medium long shot", correct: false },
+        { text: "Point of view shot", correct: false },
+        { text: "Long shot", correct: false }
+    ]
+},
+{
+    question: "It refers to intensity. It dictates whether the color appears subtle or more vibrant.",
+    answers: [
+        { text: "Saturation", correct: true },
+        { text: "Hue", correct: false },
+        { text: "Value", correct: false },
+        { text: "None of the above", correct: false }
+    ]
+},
+{
+    question: "This database type allows unstructured and semistructured data to be stored and manipulated.",
+    answers: [
+        { text: "Graph Databases", correct: false },
+        { text: "Cloud Databases", correct: false },
+        { text: "Data Warehouses", correct: false },
+        { text: "NoSQL Databases", correct: true }
+    ]
+},
+{
+    question: "It is a popular image editing software created by Adobe",
+    answers: [
+        { text: "Snapseed", correct: false },
+        { text: "Canva", correct: false },
+        { text: "Figma", correct: false },
+        { text: "Photoshop", correct: true }
+    ]
+},
+{
+    question: "A process can move between the various queues; aging can be implemented this way.",
+    answers: [
+        { text: "Multi-level Queue", correct: false },
+        { text: "Multilevel Feedback Queue", correct: true },
+        { text: "Round Robin", correct: false },
+        { text: "Shortest-Job-First", correct: false }
+    ]
+},
+{
+    question: "In this transmission mode, both stations transmit, but only one at a time",
+    answers: [
+        { text: "Simplex", correct: false },
+        { text: "Parallel transmission", correct: false },
+        { text: "Half Duplex", correct: true },
+        { text: "Full Duplex", correct: false }
+    ]
+},
+{
+    question: "Which of the following is a disadvantage of a real-time operating system?",
+    answers: [
+        { text: "Task shifting", correct: false },
+        { text: "Focus on Application", correct: false },
+        { text: "Thread Priority", correct: true },
+        { text: "Error Free", correct: false }
+    ]
+},
+{
+    question: "It is responsible for addressing messages and translating logical addresses and names into physical addresses",
+    answers: [
+        { text: "Application Layer", correct: false },
+        { text: "Network Layer", correct: true },
+        { text: "Presentation Layer", correct: false },
+        { text: "Data-link Layer", correct: false }
+    ]
+},
+{
+    question: "Which of the following is a CSS framework?",
+    answers: [
+        { text: "Bootstrap", correct: true },
+        { text: "Angular", correct: false },
+        { text: "Laravel", correct: false },
+        { text: "React", correct: false }
+    ]
+},
+{
+    question: "Let the domain of x be all plants and animals. Which is true if B(x) = 'x is a bird', E(x) = 'x lays eggs', M(x) = 'x is a mammal', P(x) = 'x is a plant', W(x) = 'x inhabits water'",
+    answers: [
+        { text: "∀x (E(x) → B(x))", correct: false },
+        { text: "∃x (W(x) ∧ ¬P(x))", correct: true },
+        { text: "∃x (W(x) ∧ P(x))", correct: false },
+        { text: "∀x (W(x) ∧ ¬M(x))", correct: false }
+    ]
+},
+{
+    question: "What does CSS stand for in web development?",
+    answers: [
+        { text: "Color Style Structure", correct: false },
+        { text: "Cascading Style Sheets", correct: true },
+        { text: "Creative Styling System", correct: false },
+        { text: "Code Styling Syntax", correct: false }
+    ]
+},
+{
+    question: "Which data structure uses the FIFO (First In, First Out) principle?",
+    answers: [
+        { text: "Stack", correct: false },
+        { text: "Queue", correct: true },
+        { text: "Tree", correct: false },
+        { text: "Graph", correct: false }
+    ]
+},
+{
+    question: "Which layer of the OSI model handles encryption and data compression?",
+    answers: [
+        { text: "Transport Layer", correct: false },
+        { text: "Network Layer", correct: false },
+        { text: "Presentation Layer", correct: true },
+        { text: "Application Layer", correct: false }
+    ]
+},
+{
+    question: "Which database language is used to define the structure of a database schema?",
+    answers: [
+        { text: "DML", correct: false },
+        { text: "DCL", correct: false },
+        { text: "DDL", correct: true },
+        { text: "TCL", correct: false }
+    ]
+}
+,
+{
+    question: "They plan the advertising, oversee the creative process, and give guidance to the people that work under them.",
+    answers: [
+        { text: "Creative/Art director", correct: true },
+        { text: "Graphic Designer", correct: false },
+        { text: "Senior Director", correct: false },
+        { text: "Visual Graphic Artist", correct: false }
     ]
 }
 ];
